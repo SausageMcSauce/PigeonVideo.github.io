3407934b217190c09fd1087590092ad9d5ab9aeb
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2020-10-02 15:49:22 +0000

    Many changes such as fixing misspellings and indenting

--- src/public/script.js
+++ src/public/script.js
@@ -1,2 +1,2 @@
 console.group("Stop!")
-console.info("Do NOT copy+paste anything here! It would allow hackers to take over your account and prevent you from acessing the website!")
+console.info("Do NOT copy+paste anything here! Some code could allow hackers to take over your account and prevent you from acessing the website!")
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use("/public", express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => { 
-console.log("Your app is listening on: " + "http://localhost:" + listener.address().port);
+  console.log("Your app is listening on: " + "http://localhost:" + listener.address().port);
 });
